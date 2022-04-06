c113645095db847c379c4cad1ac6aa145bd0a8bf
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2022-04-06 11:21:06 +0500

    Errors not fixing

--- Assets/Scripts/SettingScripts/GiftData.cs
+++ Assets/Scripts/SettingScripts/GiftData.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
 public class GiftData : MonoBehaviour
 {
     //Display data on setting
